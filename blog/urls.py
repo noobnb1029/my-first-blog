c1a64e952d2cb8b022a01d3e7edff0aaff0bb878
Antonio Velazquez <antoniovelsa@outlook.com>
2017-12-29 15:18:01 -0600

    Vistas individuales

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list), #si esta vacia, se asigna el view de la lista de posts a la pagina de inicio
+	url (r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name ='post_detail'), #<pk> significa que se tomara el # de posty se pasara como parametro a la view de post_detail (la variable se llamara pk)
 ]
