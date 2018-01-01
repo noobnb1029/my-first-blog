95615c6e2ae7c0ca6e8a026ba6b1083896b3b134
Antonio Velazquez <antoniovelsa@outlook.com>
2018-01-01 13:35:10 -0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list), #si esta vacia, se asigna el view de la lista de posts a la pagina de inicio
-	url (r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name ='post_detail'), #<pk> significa que se tomara el # de posty se pasara como parametro a la view de post_detail (la variable se llamara pk)
+	url (r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name ='post_detail'), #<pk> significa que se tomara el numero  de post y se pasara como parametro a la view de post_detail (la variable se llamara pk)
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
 ]
